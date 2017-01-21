b69a7c36d41607c44d506c0c413c7bd56179eee0
Bartłomiej Dziwoń <dziwon.bartlomiej@gmail.com>
2017-01-21 21:08:00 +0100

    dodano trochę todo

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -3,4 +3,10 @@
  */
 public class InputHandler {
     //TODO: inputHandler
+    //TODO: Case z rozpoznywaniem operacji
+    //TODO: zapis projektów na serwerze
+    //TODO: dodawanie uzytkownika do projektu
+    //TODO: dodawanie błędu do projektu
+    //TODO: tworzenie nowego projektu
+    //i tym podobne
 }
