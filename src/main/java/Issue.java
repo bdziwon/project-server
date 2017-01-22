b1a84422959916029571b2ba7e09b1e434c6a3b7
Bartłomiej Dziwoń <dziwon.bartlomiej@gmail.com>
2017-01-22 20:12:32 +0100

    dodano pole project_id i zaktualizowano metodę makeInsertSql o nowe pole

--- src/main/java/Issue.java
+++ src/main/java/Issue.java
@@ -5,6 +5,7 @@ public class Issue implements Cloneable, DatabaseSqlInterface {
 	
 	
     private int id  = -1;
+    private int projectId = -1;
     private String title        = "Brak tytułu";
     private String description  = "Brak opisu";
     private String priority     = "ZWYKŁY";
@@ -59,8 +60,8 @@ public class Issue implements Cloneable, DatabaseSqlInterface {
     @Override
     public String makeInsertSql() {
         String sql =
-                "INSERT INTO issue(title,description,priority) " +
-                        "VALUES ('"+getTitle()+"','"+getDescription()+"','"+getPriority()+"')";
+                "INSERT INTO issue(id_project,title,description,priority) " +
+                        "VALUES ('"+getProjectId()+"','"+getTitle()+"','"+getDescription()+"','"+getPriority()+"')";
 
         return sql;
     }
@@ -69,5 +70,13 @@ public class Issue implements Cloneable, DatabaseSqlInterface {
     public int setId(int id) {
         this.id = id;
         return this.id;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
     }
 }
