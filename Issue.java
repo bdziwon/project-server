10e4bfe8ad14a51ca8d89321e21daaa5bc5cedcd
mikolajczaja <pridak136@op.pl>
2017-01-22 12:04:33 +0100

    Add files via upload
    oppis

---
+++ Issue.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Bartłomiej Dziwoń on 21.01.2017.
+ */
+public class Issue implements Cloneable {
+	
+	
+    private int id;
+    private String title = null;
+    private String description = null;
+    private String priority = null;    
+    
+    public String getTitle(){
+    return this.title;
+    }
+
+    public void setTitle(String title){
+    this.title=title;
+    }
+    
+    public String getDescription(){
+    return this.description;
+    }    
+       
+    public void setDescription(String description){
+    this.description=description;
+    }     
+    
+    public String getPriority(){
+    return this.priority;
+    }    
+       
+    public void setPriority(String priority){
+    this.priority=priority;
+    }      
+    
+}
