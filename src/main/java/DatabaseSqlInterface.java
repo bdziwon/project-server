ef279a0e22cb11a2947884da0fd7031b874cb386
Bartłomiej Dziwoń <dziwon.bartlomiej@gmail.com>
2017-01-22 14:17:20 +0100

    Dodano metody update i delete

--- src/main/java/DatabaseSqlInterface.java
+++ src/main/java/DatabaseSqlInterface.java
@@ -2,5 +2,7 @@
  * Created by Bartłomiej Dziwoń on 22.01.2017.
  */
 public interface DatabaseSqlInterface {
+    public String makeUpdateSql();
+    public String makeDeleteSql();
     public String makeInsertSql();
 }
