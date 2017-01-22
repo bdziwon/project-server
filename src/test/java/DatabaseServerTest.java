1a6b52cdc7e38338e4eb70c8018e51f977b8c1d3
Bartłomiej Dziwoń <dziwon.bartlomiej@gmail.com>
2017-01-22 16:04:28 +0100

    Dodano testy do update oraz dla każdej tabeli

--- src/test/java/DatabaseServerTest.java
+++ src/test/java/DatabaseServerTest.java
@@ -46,23 +46,66 @@ public class DatabaseServerTest {
         //create tables
         db.createTablesIfDoesNotExists();
 
-        //TODO: insert
+        //insert
         User user = new User();
-        user.setName("Bartek");
-        user.setSurname("Dz");
-        user.setJobTitle("PROGRAMISTA");
+        Project project = new Project();
+        Issue issue = new Issue();
+        {
+            user.setName("Bartek");
+            user.setSurname("Dz");
+            user.setJobTitle("PROGRAMISTA");
+            int id = user.getId();
+            user = (User) db.insert(user);
+            int newId = user.getId();
+            assertThat(id).isNotEqualTo(newId);
+            assertThat(newId).isGreaterThan(0);
+        }
+        {
+            issue.setTitle("title");
+            int id = issue.getId();
+            issue = (Issue) db.insert(issue);
+            int newId = issue.getId();
+            assertThat(id).isNotEqualTo(newId);
+            assertThat(newId).isGreaterThan(0);
+        }
+        {
+            project.setTitle("title");
+            int id = project.getId();
+            project = (Project) db.insert(project);
+            int newId = project.getId();
+            assertThat(id).isNotEqualTo(newId);
+            assertThat(newId).isGreaterThan(0);
+        }
 
-        int id = user.getId();
-        user = (User) db.insert(user);
-        int newId = user.getId();
-        assertThat(id).isNotEqualTo(newId);
-        assertThat(newId).isGreaterThan(0);
-
-        //TODO: update
-
-        //TODO: delete
-        int changes = db.delete(user);
-        assertThat(changes).isGreaterThan(0);
-
+        //update
+        {
+            user.setName("Mikołaj");
+            int changes = db.update(user);
+            assertThat(changes).isGreaterThan(0);
+        }
+        {
+            project.setDescription("New description");
+            int changes = db.update(project);
+            assertThat(changes).isGreaterThan(0);
+        }
+        {
+            issue.setDescription("New description");
+            int changes = db.update(issue);
+            assertThat(changes).isGreaterThan(0);
+        }
+        
+        //delete
+        {
+            int changes = db.delete(user);
+            assertThat(changes).isGreaterThan(0);
+        }
+        {
+            int changes = db.delete(project);
+            assertThat(changes).isGreaterThan(0);
+        }
+        {
+            int changes = db.delete(issue);
+            assertThat(changes).isGreaterThan(0);
+        }
     }
 }
