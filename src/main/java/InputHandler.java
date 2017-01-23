d07b4baee50471b35c5f0b68a3f272f2e2da8dfc
Bartłomiej Dziwoń <dziwon.bartlomiej@gmail.com>
2017-01-23 17:45:27 +0100

    Operacje na bazie danych dla przychodzących obiektów

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -1,13 +1,50 @@
-/**
- * Created by Bartłomiej Dziwoń on 21.01.2017.
- */
+
 public class InputHandler {
-    //TODO: inputHandler
+
+    public Object handle(Object object) {
+        //case...
+        return object;
+    }
+
+    private Object insert(Object object) {
+            // to stworzy dowolny projekt, błąd lub użytkownika, wystarczy żeby rozróżnić że to insert
+            DatabaseServer db = DatabaseServer.getInstance();
+            object = db.insert(object);
+
+            //zwraca obiekt z id z bazy, nie dodaje issue i user do projektu jeśli na ma liście, to robi dopiero update dla projektu
+            return object;
+    }
+
+    private Object update(Object object) {
+        //update
+        //jeśli aktualizujemy projekt to stworzą się automatycznie nieistniejący użytkownicy i błędy
+        DatabaseServer db = DatabaseServer.getInstance();
+        object = db.update(object);
+        return object;
+    }
+
+    /**
+     * Usuwanie z bazy po id w obiekcie
+     * Jeśli usuwamy projekt usunie też jego błędy, użytkownicy zostają
+     * @param object obiekt klasy {@link Issue} {@link Project} {@link User}
+     * @return ilość zmian w bazie, jak zero to błąd
+     */
+    private Object delete(Object object) {
+        DatabaseServer db = DatabaseServer.getInstance();
+        return db.delete(object);
+    }
+
+    /**
+     * Pobiera dane z bazy do obiektu
+     * Jeśli to projekt, pobierze także wszystkie błędy i użytkowników do list
+     * @param object
+     * @return Obiekt reprezentujący dane z bazy
+     */
+    private Object select(Object object) {
+        DatabaseServer db = DatabaseServer.getInstance();
+        return db.select(object);
+    }
     //TODO: Case z rozpoznywaniem operacji
-    //TODO: zapis projektów na serwerze
-    //TODO: dodawanie uzytkownika do projektu
-    //TODO: dodawanie błędu do projektu
-    //TODO: tworzenie nowego projektu
+    //TODO: zapis projektu (plików) na serwerze
     //TODO: String w żadnym obiekcie nie może zawierac apostrofu '
-    //i tym podobne
 }
