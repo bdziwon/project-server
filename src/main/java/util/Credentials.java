e944a8d7380be9c389c5a6682de62fd51204b6be
Bartłomiej Dziwoń <xpxpria@gmail.com>
2017-01-27 20:28:16 +0100

    Dodano klasę Credentials

---
+++ src/main/java/util/Credentials.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.io.Serializable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Credentials implements Serializable {
+    private String login;
+    private String password;
+
+    public Credentials(String login, String password) {
+        this.login    = login;
+        this.password = password;
+    }
+
+    public boolean loginContainsUnallowedChars() {
+        Pattern pattern = Pattern.compile("[^a-z0-9]", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(login);
+        if (matcher.find()) {
+            return true;
+        }
+        return  false;
+    }
+
+    public boolean passwordContainsUnallowedChars() {
+        Pattern pattern = Pattern.compile("[^a-z0-9_]", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(password);
+        if (matcher.find()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean loginHaveAllowedLength() {
+        if (login.length() < 4) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean passwordHaveAllowedLength() {
+        if (password.length() < 4) {
+            return false;
+        }
+        return true;
+    }
+
+}
